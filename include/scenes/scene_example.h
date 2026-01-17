898cc509eb83a44f78f27364cf37e27ee959e1cb
uint <abhinav.prsai@gmail.com>
2026-01-17 15:21:17 +0000

    add scene_play

--- include/scenes/scene_example.h
+++
@@ -1,7 +0,0 @@
-#ifndef SCENE_EXAMPLE_H
-#define SCENE_EXAMPLE_H
-
-extern struct scene scene_example;
-
-#endif /* SCENE_EXAMPLE_H */
-
---
+++ include/scenes/scene_play.h
@@ -0,0 +1,9 @@
+#ifndef SCENE_PLAY_H
+#define SCENE_PLAY_H
+
+#include "scene.h"
+
+extern struct scene scene_play;
+
+#endif /* SCENE_PLAY_H */
+
