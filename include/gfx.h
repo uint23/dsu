5aeece1a3e47bde70010364503830a7795532ea7
uint <abhinav.prsai@gmail.com>
2026-01-17 10:37:15 +0000

    circles use floating radius

--- include/gfx.h
+++ include/gfx.h
@@ -8,7 +8,7 @@ void gfx_clear(u16 col);
 /* draw */
 void gfx_draw_px(int x, int y, u16 col);
 void gfx_draw_line(int x0,int y0,int x1,int y1,u16 c);
-void gfx_draw_circle(int cx, int cy, int r, u16 col);
+void gfx_draw_circle(int cx, int cy, float r, u16 col);
 
 #endif /* GFX_H */
 
--- source/gfx.c
+++ source/gfx.c
@@ -44,9 +44,9 @@ void gfx_draw_line(int x0,int y0,int x1,int y1,u16 c)
     }
 }
 
-void gfx_draw_circle(int cx, int cy, int r, u16 col)
+void gfx_draw_circle(int cx, int cy, float r, u16 col)
 {
-	int x = r;
+	float x = r;
 	int y = 0;
 
 	int err = 0;
